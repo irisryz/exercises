1f089c7b6aae21c7252ee5723a688e5329c372fc
irisryz <zhang.irisr@gmail.com>
2025-02-23 20:50:21 -0800

    Create repeatSub.java

---
+++ repeatSub.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        boolean isGood;
+        ArrayList<Integer> listDivisors = new ArrayList<>();
+
+        // Find all divisors of s.length()
+        for (int i = 1; i <= s.length() / 2; i++) { // Fix the range
+            if (s.length() % i == 0) {
+                listDivisors.add(i);
+            }
+        }
+
+        for (int j : listDivisors) {
+            System.out.println("Checking divisor: " + j);
+            String ref = s.substring(0, j); 
+            isGood = true;
+
+            for (int k = j; k < s.length(); k += j) { 
+                if (!s.substring(k, k + j).equals(ref)) {
+                    System.out.println("Mismatch found at " + k);
+                    isGood = false;
+                    break;
+                }
+            }
+
+            if (isGood) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
